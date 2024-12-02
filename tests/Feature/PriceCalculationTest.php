a6dbfdff6a36a954d0066bf9c091ef02022d4e58
Daniel Verner <danielv2000@gmail.com>
2024-12-02 15:37:12 +0100

    finished pricing feature test

--- tests/Feature/PriceCalculationTest.php
+++ tests/Feature/PriceCalculationTest.php
@@ -2,6 +2,18 @@
 
 use App\Models\CarrierService;
 
+test('validates required fields for price calculation', function () {
+    $this->postJson(route('pricing.calculate'), [])
+        ->assertStatus(422)
+        ->assertJsonValidationErrors([
+            'carrier_service_id',
+            'sender_country_code',
+            'recipient_country_code',
+            'weight',
+        ]);
+});
+
+
 test('calculates correct prices for PostNL Parcel', function () {
 
     $this->seed();
@@ -34,6 +46,34 @@ test('calculates correct prices for PostNL Parcel', function () {
         ])
         ->assertStatus(200)
         ->assertJson(['price' => 10.99]);
+});
+
+test('calculating international prices for PostNL Parcel does not exist', function () {
+
+    $this->seed();
+
+    $carrierServiceId = CarrierService::where('name', 'PostNL Parcel')->first()->id;
+
+    $this->postJson(route('pricing.calculate'), [
+            'carrier_service_id' => $carrierServiceId,
+            'sender_country_code' => 'NL',
+            'recipient_country_code' => 'US',
+            'weight' => 0.5,
+        ])->assertStatus(404);
+
+    $this->postJson(route('pricing.calculate'), [
+            'carrier_service_id' => $carrierServiceId,
+            'sender_country_code' => 'US',
+            'recipient_country_code' => 'NL',
+            'weight' => 5,
+        ])->assertStatus(404);
+
+    $this->postJson(route('pricing.calculate'), [
+            'carrier_service_id' => $carrierServiceId,
+            'sender_country_code' => 'NL',
+            'recipient_country_code' => 'US',
+            'weight' => 12,
+        ])->assertStatus(404);
 });
 
 test('calculates correct prices for DHL Express (domestic)', function () {
@@ -73,7 +113,6 @@ test('calculates correct prices for DHL Express (domestic)', function () {
 test('calculates correct prices for DHL Express (international)', function () {
 
     $this->seed();
-
     $carrierServiceId = CarrierService::where('name', 'DHL Express')->first()->id;
 
     $this->postJson(route('pricing.calculate'), [
